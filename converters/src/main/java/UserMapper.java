16f100ffa6bc37a66b3d198cfd79a754f2446d1f
mdgreen18 <matthewdgreen18@yahoo.com>
2020-11-17 23:11:58 -0500

    converters assignment

---
+++ converters/src/main/java/UserMapper.java
@@ -0,0 +1,13 @@
+import guru.springframework.domain.UserCommand;
+import guru.springframework.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserCommand userToUserCommand(User user);
+
+    User userCommandToUser(UserCommand userCommand);
+}
